c67e4730b3982f1a6db2df95460a22b5e2905518
Natalia-D <89741596+Natalia-D@users.noreply.github.com>
2021-09-06 13:00:37 +0200

    Add files via upload

--- Palindrome.js
+++ Palindrome.js
@@ -1,16 +1,18 @@
 function isPalindrome (str){
 
-    let result = "is not";
+    let result = "";
 
     str = str.split (' ').join('').toLowerCase();
 
 for (let i=0;  i < str.length; i++){
-    if (str[i] === str[str.length -1 - i])
-     {result = "is";}
+   if (str[i] == str[str.length -1 - i])
+     { result = "is";}
     else
      {result = "is not";}
+ if (result == "is not"){ break;}
 }
- return result;
+ 
+return result;
 }
 
 input = "А роза упала на лапу Азора";
